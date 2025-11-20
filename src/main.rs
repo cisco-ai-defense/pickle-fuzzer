b263a31edf9285b4c0906eb4f5576df53d83d6be
berisher <berisher@cisco.com>
2025-11-20 10:31:35 -0500

    nitpickery

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+//
 // Copyright 2025 Cisco Systems, Inc. and its affiliates
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +13,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-// SPDX-License-Identifier: Apache-2.0
 
 use clap::Parser;
 use color_eyre::Result;
